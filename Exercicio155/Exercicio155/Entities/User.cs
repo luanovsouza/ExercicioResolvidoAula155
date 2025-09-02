5ac7521b3fa7c585d5969818b31def3acfb5c209
Luan Oliveira <luanvisouza037@gmail.com>
2025-09-02 17:59:42 -0300

    Initial Commit

---
+++ Exercicio155/Exercicio155/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio155.Entities;
+
+public class User
+{
+    public string? Name { get; set; }
+    public DateTime AcessTime { get; set; }
+
+    public User(string? name, DateTime acessTime)
+    {
+        Name = name;
+        AcessTime = acessTime;
+    }
+}
---
+++ Exercicio155/Exercicio155/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using Exercicio155.Entities;
+
+namespace Exercicio155;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string finalpath =
+            @"C:\Users\gamer\OneDrive\Documentos\Curso C# POO UDEMY\Exercicios\Seção13\Exercicio ResolvidoAula155\ExercicioResolvidoAula155/in.txt";
+
+        HashSet<User> users = new HashSet<User>();
+        
+        try
+        {
+            using (StreamReader sr = new StreamReader(finalpath))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()!) != null)
+                {
+                    string[] fields = line.Split(' ');
+                    string userName = fields[0];
+                    DateTime acessTime = DateTime.ParseExact(fields[1], "yyyy-MM-ddTHH:mm:ssK", CultureInfo.InvariantCulture);
+                    
+                    
+                    
+                    users.Add(new User(userName, acessTime));
+                }
+
+                Console.WriteLine("Leitura do Arquivo Feita com Sucesso!");
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"Total users: {users.Count}");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Um erro aconteceu!");
+            Console.WriteLine(e.Message);
+        }
+        
+    }
+}
